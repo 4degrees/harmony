f3e034745b5ca6afb2137af66e32eaafa7ae2137
Martin Pengelly-Phillips <martin@4degrees.ltd.uk>
2013-08-16 15:12:40 +0100

    Move merge function to processor that requires it.
    In preparation for refactor of merge logic that will require non-general
    information.

--- source/harmony/schema/__init__.py
+++ source/harmony/schema/__init__.py
@@ -2,28 +2,3 @@
 # :copyright: Copyright (c) 2013 Martin Pengelly-Phillips
 # :license: See LICENSE.txt.
 
-import copy
-
-
-def merge(target, reference):
-    '''Merge *reference* dictionary into *target* dictionary.
-
-    *target* has precedence in the merge which follows the rules:
-
-        * New keys in *reference* are added to *target*.
-        * Keys already present in *target* are not overwritten.
-        * When the same key exists in both and the value is a dictionary, the
-          dictionaries are also merged.
-
-    '''
-    for key, value in reference.items():
-
-        if not key in target:
-            # Copy value to target
-            target[key] = copy.deepcopy(value)
-
-        else:
-            # Merge if both target and reference value is a dictionary
-            if isinstance(value, dict) and isinstance(target[key], dict):
-                merge(target[key], value)
-
--- source/harmony/schema/processor.py
+++ source/harmony/schema/processor.py
@@ -2,10 +2,10 @@
 # :copyright: Copyright (c) 2013 Martin Pengelly-Phillips
 # :license: See LICENSE.txt.
 
+import copy
 import urlparse
 from abc import ABCMeta, abstractmethod
 
-import harmony.schema
 from harmony.schema.validator import validator_for
 
 
@@ -115,5 +115,26 @@ class MixinProcessor(Processor):
             self._process(mixin, schemas)
 
             # Merge mixin into the referring fragment.
-            harmony.schema.merge(fragment, mixin)
+            self._merge(fragment, mixin)
 
+    def _merge(self, target, reference):
+        '''Merge *reference* dictionary into *target* dictionary.
+
+        *target* has precedence in the merge which follows the rules:
+
+            * New keys in *reference* are added to *target*.
+            * Keys already present in *target* are not overwritten.
+            * When the same key exists in both and the value is a dictionary,
+              the dictionaries are also merged.
+
+        '''
+        for key, value in reference.items():
+
+            if not key in target:
+                # Copy value to target
+                target[key] = copy.deepcopy(value)
+
+            else:
+                # Merge if both target and reference value is a dictionary
+                if isinstance(value, dict) and isinstance(target[key], dict):
+                    self._merge(target[key], value)
