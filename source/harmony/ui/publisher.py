aeed9c7d3fb8ceab6d8aa1bdb80f57bde0ac03cf
Martin Pengelly-Phillips <martin@4degrees.ltd.uk>
2013-08-28 22:55:44 +0100

    Refactor demo into publisher base.

---
+++ source/harmony/ui/publisher.py
@@ -0,0 +1,116 @@
+# :coding: utf-8
+# :copyright: Copyright (c) 2013 Martin Pengelly-Phillips
+# :license: See LICENSE.txt.
+
+from PySide import QtGui
+
+import harmony.ui.error_tree
+
+
+class Publisher(QtGui.QDialog):
+    '''Publisher dialog.'''
+
+    def __init__(self, session, factory, parent=None):
+        '''Initialise with *session*, widget *factory* and *parent*.
+
+        *session* should be a :py:class:`~harmony._session.Session` instance
+        providing access to available schemas.
+
+        *factory* should be a :py:class:`~harmony.ui.widget._factory.Factory`
+        instance responsible for returning appropriate widgets for schema
+        fragments.
+
+        *parent* is the optional owner of this UI element.
+
+        '''
+        super(Publisher, self).__init__(parent=parent)
+
+        self._session = session
+        self._factory = factory
+
+        self._construct()
+        self._postConstruction()
+
+    def _construct(self):
+        '''Construct widget.'''
+        self.setLayout(QtGui.QVBoxLayout())
+
+        self._schemaSelector = QtGui.QComboBox()
+        self._schemaDetailsArea = QtGui.QScrollArea()
+        self._schemaDetailsArea.setWidgetResizable(True)
+
+        self.layout().addWidget(self._schemaSelector, stretch=0)
+        self.layout().addWidget(self._schemaDetailsArea, stretch=1)
+
+    def _postConstruction(self):
+        '''Perform post-construction operations.'''
+        self.setWindowTitle('Harmony Publisher')
+
+        self._schemaSelector.currentIndexChanged.connect(self._onSelectSchema)
+
+        for schema in sorted(self._filterSchemas(self._session.schemas)):
+            self._schemaSelector.addItem(
+                schema.get('title', schema['id']),
+                schema
+            )
+
+    def _filterSchemas(self, schemas):
+        '''Return a list of *schemas* to display as options in the selector.'''
+        filtered = []
+        for schema in schemas:
+            if schema.get('id', '').startswith('harmony:/item/'):
+                filtered.append(schema)
+
+        return filtered
+
+    def _onSelectSchema(self, index):
+        '''Handle schema selection.'''
+        # Cleanup any existing schema widgets.
+        existingSchemaDetails = self._schemaDetailsArea.takeWidget()
+        if existingSchemaDetails is not None:
+            existingSchemaDetails.setParent(None)
+            existingSchemaDetails.deleteLater()
+
+        # Construct new schema widgets.
+        schema = self._schemaSelector.itemData(index)
+        schemaDetails = self._factory(schema)
+        schemaDetails.setRequired(True)
+        schemaDetails.setContentsMargins(5, 5, 5, 5)
+        schemaDetails.setFrameStyle(QtGui.QFrame.NoFrame)
+
+        self._schemaDetailsArea.setFrameStyle(
+            QtGui.QFrame.StyledPanel | QtGui.QFrame.Plain
+        )
+
+        self._schemaDetailsArea.setWidget(schemaDetails)
+
+        # Connect dynamic validation.
+        schemaDetails.valueChanged.connect(self._onValueChanged)
+
+        # Construct initial data and set.
+        instance = self._session.instantiate(schema)
+        schemaDetails.setValue(instance)
+
+    def _onValueChanged(self):
+        '''Handle change in value.'''
+        schema = self._schemaSelector.itemData(
+            self._schemaSelector.currentIndex()
+        )
+
+        if not schema:
+            return
+
+        instance = self._schemaDetailsArea.widget().value()
+        self.validate(instance, schema)
+
+    def validate(self, instance, schema):
+        '''Validate *instance* against *schema* and update UI state.'''
+        # Validate
+        errors = self._session.validate(instance)
+
+        # Construct error tree that maps errors to UI structure.
+        error_tree = harmony.ui.error_tree.ErrorTree(errors)
+
+        # Apply errors to interface.
+        self._schemaDetailsArea.widget().setError(error_tree)
+
--- test/interactive/publisher.py
+++ test/interactive/publisher.py
@@ -11,99 +11,7 @@ import jsonpointer
 import harmony.session
 import harmony.ui.widget.factory
 import harmony.ui.error_tree
-
-
-class Demo(QtGui.QDialog):
-    '''Demo dialog.'''
-
-    def __init__(self, session, parent=None):
-        '''Initialise demo with *session* and *parent*.'''
-        super(Demo, self).__init__(parent=parent)
-
-        self.session = session
-        self.widget_factory = Factory(self.session)
-
-        self.construct()
-        self.post_construction()
-
-    def construct(self):
-        '''Construct widget.'''
-        self.setLayout(QtGui.QVBoxLayout())
-
-        self.schema_selector = QtGui.QComboBox()
-        self.schema_details_area = QtGui.QScrollArea()
-        self.schema_details_area.setWidgetResizable(True)
-
-        self.layout().addWidget(self.schema_selector, stretch=0)
-        self.layout().addWidget(self.schema_details_area, stretch=1)
-
-    def post_construction(self):
-        '''Perform post-construction operations.'''
-        self.setWindowTitle('Harmony UI Builder')
-
-        self.schema_selector.currentIndexChanged.connect(self.on_select_schema)
-
-        # Filter schemas
-        schemas = []
-        for schema in self.session.schemas:
-            if schema.get('id', '').startswith('harmony:/item/'):
-                schemas.append(schema)
-
-        for schema in sorted(schemas):
-            self.schema_selector.addItem(
-                schema.get('title', schema['id']),
-                schema
-            )
-
-    def on_select_schema(self, index):
-        '''Handle schema selection.'''
-        # Cleanup any existing schema widgets.
-        existing_schema_details = self.schema_details_area.takeWidget()
-        if existing_schema_details is not None:
-            existing_schema_details.setParent(None)
-            existing_schema_details.deleteLater()
-
-        # Construct new schema widgets.
-        schema = self.schema_selector.itemData(index)
-        schema_details = self.widget_factory(schema)
-        schema_details.setRequired(True)
-        schema_details.setContentsMargins(5, 5, 5, 5)
-        schema_details.setFrameStyle(QtGui.QFrame.NoFrame)
-
-        self.schema_details_area.setFrameStyle(
-            QtGui.QFrame.StyledPanel | QtGui.QFrame.Plain
-        )
-
-        self.schema_details_area.setWidget(schema_details)
-
-        # Connect dynamic validation.
-        schema_details.valueChanged.connect(self.on_value_changed)
-
-        # Construct initial data and set.
-        instance = self.session.instantiate(schema)
-        schema_details.setValue(instance)
-
-    def on_value_changed(self):
-        '''Handle change in value.'''
-        schema = self.schema_selector.itemData(
-            self.schema_selector.currentIndex()
-        )
-
-        if not schema:
-            return
-
-        instance = self.schema_details_area.widget().value()
-        self.validate(instance, schema)
-
-    def validate(self, instance, schema):
-        '''Validate *instance* against *schema* and update UI state.'''
-        # Validate
-        errors = self.session.validate(instance)
-
-        # Construct error tree that maps errors to UI structure.
-        error_tree = harmony.ui.error_tree.ErrorTree(errors)
-
-        self.schema_details_area.widget().setError(error_tree)
+import harmony.ui.publisher
 
 
 class Factory(harmony.ui.widget.factory.Factory):
@@ -223,7 +131,8 @@ def main(arguments=None):
     application = QtGui.QApplication(arguments)
 
     session = harmony.session.Session()
-    dialog = Demo(session)
+    factory = Factory(session)
+    dialog = harmony.ui.publisher.Publisher(session, factory)
     dialog.resize(600, 800)
     dialog.show()
 
